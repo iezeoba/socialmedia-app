8b6f49f0df699d0e038cf6893f6a0336fb9f846b
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2021-02-15 23:54:26 -0700

    refactored codes for resolvers

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -4,16 +4,67 @@ const { UserInputError } = require("apollo-server");
 
 const User = require("../../models/User");
 const { SECRET_KEY } = require("../../config");
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../util/validators");
+
+const generateToken = (user) => {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+};
 
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+      const user = await User.findOne({ username });
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("User not found", { errors });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "Wrong credentials";
+        throw new UserInputError("Wrong credentials", { errors });
+      }
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
+
     async register(
       _,
       { registerInput: { username, email, password, confirmPassword } },
       context,
       info
     ) {
-      const user = await User.findOne({ username });
+      //validating user registration data/inputs//
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+      //checking that user does not exist already//
+      const user = await User.findOne({ username }); //collection.findOne() is a mongodb method which returns a Promise that resolves to the first document in the collection that matches the query filter//
       if (user) {
         throw new UserInputError("This username already exists", {
           errors: {
@@ -32,15 +83,8 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        SECRET_KEY,
-        { expiresIn: "1h" }
-      );
+      const token = generateToken(res);
+
       return {
         ...res._doc,
         id: res._id,
