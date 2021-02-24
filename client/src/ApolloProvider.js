31f117d421551fa41f4dd673d1731978cc2107bb
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2021-02-23 19:07:27 -0700

    set context headers for authorization

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -9,24 +9,24 @@ import {
   useQuery,
   gql,
 } from "@apollo/client";
-// import { setContext } from '@apollo/client';
+import { setContext } from "@apollo/client";
 
 const httpLink = new HttpLink({
   uri: "http://localhost:5000",
 });
 
-// const authLink = setContext(() => {
-//   const token = localStorage.getItem('jwtToken');
-//   return {
-//     headers: {
-//       Authorization: token ? `Bearer ${token}` : ''
-//     }
-//   };
-// });
+const authLink = setContext(() => {
+  const token = localStorage.getItem("jwtToken");
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  //link: authLink.concat(httpLink),
-  link: httpLink,
+  link: authLink.concat(httpLink),
+  // link: httpLink,
   cache: new InMemoryCache(),
 });
 
