1a88e0df04ecaa1238595853ef1b1c4a3512852f
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2021-03-02 23:54:12 -0700

    implemented setContext in Provider

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
   useQuery,
   gql,
 } from "@apollo/client";
-import { setContext } from "@apollo/client";
+import { setContext } from "apollo-link-context";
 
 const httpLink = new HttpLink({
   uri: "http://localhost:5000",
