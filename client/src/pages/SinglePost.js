5acd2545efd4c8c0f02ff5e411b4f109fdec1564
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2021-03-04 22:43:03 -0700

    fetch individual post functionality

---
+++ client/src/pages/SinglePost.js
@@ -0,0 +1,117 @@
+import React, { useContext } from "react";
+import { useQuery, gql } from "@apollo/client";
+import { Card, Grid, Image, Button, Icon, Label } from "semantic-ui-react";
+import moment from "moment";
+import { AuthContext } from "../context/Auth";
+
+import LikeButton from "../components/LikeButton";
+import DeleteButton from "./DeleteButton";
+
+const SinglePost = (props) => {
+  const postId = props.match.params.postId;
+  const { user } = useContext(AuthContext);
+  console.log(postId);
+
+  const { data: { getPost } = {} } = useQuery(FETCH_POST_QUERY, {
+    variables: { postId },
+  });
+
+  const deletePostCallback = () => {
+    props.history.push("/");
+  };
+
+  let postMarkup;
+  if (!getPost) {
+    postMarkup = <p>Loading Post...</p>;
+  } else {
+    const {
+      id,
+      body,
+      createdAt,
+      username,
+      comments,
+      likes,
+      likeCount,
+      commentCount,
+    } = getPost;
+
+    postMarkup = (
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={2}>
+            <Image
+              src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+              size="small"
+              float="right"
+            />
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <Card fluid>
+              <Card.Content>
+                <Card.Header>{username}</Card.Header>
+                <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+                <Card.Description>{body}</Card.Description>
+              </Card.Content>
+              <hr />
+              <Card.Content extra>
+                <LikeButton user={user} post={{ id, likeCount, likes }} />
+                <Button
+                  as="div"
+                  labelPosition="right"
+                  onClick={() => console.log("Comment on Post")}
+                >
+                  <Button basic color="blue">
+                    <Icon name="comments" />
+                  </Button>
+                  <Label basic color="blue" pointing="left">
+                    {commentCount}
+                  </Label>
+                </Button>
+                {user && user.username === username && (
+                  <DeleteButton postId={id} callback={deletePostCallback} />
+                )}
+              </Card.Content>
+            </Card>
+            {comments.map((comment) => (
+              <Card fluid key={comment.id}>
+                <Card.Content>
+                  {user && user.username === comment.username && (
+                    <DeleteButton postId={id} commentId={comment.id} />
+                  )}
+                  <Card.Header>{comment.username}</Card.Header>
+                  <Card.Meta>{moment(comment.createdAt).fromNow()}</Card.Meta>
+                  <Card.Description>{comment.body}</Card.Description>
+                </Card.Content>
+              </Card>
+            ))}
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
+  return postMarkup;
+};
+
+const FETCH_POST_QUERY = gql`
+  query($postId: ID!) {
+    getPost(postId: $postId) {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
+
+export default SinglePost;
